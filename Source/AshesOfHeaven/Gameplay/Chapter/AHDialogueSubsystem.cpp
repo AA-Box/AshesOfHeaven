99ee567019f1f951d64ffd85a32d36f52e827a5d
Seyed Ahmad Sarollahi <ahmad.sarollahi@gmail.com>
2026-08-24 19:52:35 +0200

    feat(level1): integrate canonical stage dialogue flow

--- Source/AshesOfHeaven/Gameplay/Chapter/AHDialogueSubsystem.cpp
+++ Source/AshesOfHeaven/Gameplay/Chapter/AHDialogueSubsystem.cpp
@@ -2,7 +2,72 @@
 #include "AshesOfHeaven.h"
 #include "Gameplay/Audio/AHAudioSubsystem.h"
 #include "Gameplay/Chapter/AHChapterSubsystem.h"
+#include "Gameplay/Chapter/AHLevelOneNarrative.h"
+#include "Engine/GameInstance.h"
 #include "Engine/World.h"
+
+void UAHDialogueSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	if (GetWorld() && GetWorld()->GetGameInstance())
+	{
+		if (UAHChapterSubsystem* Chapter = GetWorld()->GetGameInstance()->GetSubsystem<UAHChapterSubsystem>())
+		{
+			Chapter->OnStageChanged.AddDynamic(this, &UAHDialogueSubsystem::HandleChapterStageChanged);
+		}
+	}
+}
+
+void UAHDialogueSubsystem::Deinitialize()
+{
+	if (GetWorld())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(LineTimer);
+		GetWorld()->GetTimerManager().ClearTimer(StageEntryTimer);
+		if (GetWorld()->GetGameInstance())
+		{
+			if (UAHChapterSubsystem* Chapter = GetWorld()->GetGameInstance()->GetSubsystem<UAHChapterSubsystem>())
+			{
+				Chapter->OnStageChanged.RemoveDynamic(this, &UAHDialogueSubsystem::HandleChapterStageChanged);
+			}
+		}
+	}
+	Super::Deinitialize();
+}
+
+void UAHDialogueSubsystem::HandleChapterStageChanged(EAHChapterStage Stage)
+{
+	if (!GetWorld())
+	{
+		return;
+	}
+
+	GetWorld()->GetTimerManager().ClearTimer(StageEntryTimer);
+	GetWorld()->GetTimerManager().SetTimer(
+		StageEntryTimer,
+		FTimerDelegate::CreateWeakLambda(this, [this, Stage]()
+		{
+			StartStageEntrySequence(Stage);
+		}),
+		0.05f,
+		false);
+}
+
+void UAHDialogueSubsystem::StartStageEntrySequence(EAHChapterStage Stage)
+{
+	if (!GetWorld() || HasActiveDialogue())
+	{
+		return;
+	}
+
+	FName SequenceId = NAME_None;
+	TArray<FAHDialogueLine> Lines;
+	if (AHLevelOneNarrative::BuildStageEntrySequence(Stage, SequenceId, Lines))
+	{
+		StartSequence(SequenceId, Lines, true);
+	}
+}
 
 void UAHDialogueSubsystem::StartSequence(FName SequenceId, const TArray<FAHDialogueLine>& Lines, bool bOneShot)
 {
@@ -26,13 +91,16 @@ void UAHDialogueSubsystem::StartSequence(FName SequenceId, const TArray<FAHDialo
 		}
 	}
 
+	TArray<FAHDialogueLine> CanonicalLines;
+	const bool bCanonicalLevelOneSequence = AHLevelOneNarrative::ResolveDirectorSequence(SequenceId, CanonicalLines);
+
 	GetWorld()->GetTimerManager().ClearTimer(LineTimer);
-	QueuedLines = Lines;
+	QueuedLines = bCanonicalLevelOneSequence ? MoveTemp(CanonicalLines) : Lines;
 	CurrentSequenceId = SequenceId;
 	CurrentLineIndex = INDEX_NONE;
 	bActive = true;
 	#if !UE_BUILD_SHIPPING
-	UE_LOG(LogAshesOfHeaven, Display, TEXT("[Phase3.2][Dialogue] begin sequence=%s lines=%d"), *SequenceId.ToString(), Lines.Num());
+	UE_LOG(LogAshesOfHeaven, Display, TEXT("[Phase3.2][Dialogue] begin sequence=%s lines=%d canonical_level1=%s"), *SequenceId.ToString(), QueuedLines.Num(), bCanonicalLevelOneSequence ? TEXT("true") : TEXT("false"));
 	#endif
 	ShowNextLine();
 }
